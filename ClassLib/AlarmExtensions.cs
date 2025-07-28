5270f5033c17daf5dd7a1e4b1631a2ac18df85d6
legrab <b.geller@advanced-engineering.at>
2025-07-28 12:15:37 +0200

    chore: add description of the method to implement

--- ClassLib/AlarmExtensions.cs
+++ ClassLib/AlarmExtensions.cs
@@ -4,6 +4,12 @@ namespace ClassLib;
 
 public static class AlarmExtensions
 {
+    // TODO: Implement this method
+    /// <summary>
+    /// Converts an array of <see cref="AlarmBase"/> into a <see cref="ReadOnlyDictionary{TKey, TValue}"/>  where alarms are grouped by <see cref="AlarmType"/>.
+    /// Each group's values are set as <typeparamref name="TValue"/>  when the type of <see cref="AlarmBase.Value"/> matches, or set to null otherwise.
+    /// Ensures all <see cref="AlarmType"/> enum values are included, even if no alarms exist for a specific type.
+    /// </summary>
     public static ReadOnlyDictionary<AlarmType, List<Alarm<TValue>>> AsGroupedAlarmDictionary<TValue>(this AlarmBase[] alarms)
     {
         // TODO: Implement this method
